3963f2d43836c1ccc7ea1d025d80b195fae9cd18
Kazuki_Kamizuru <k.amago0721@gmail.com>
2022-08-06 18:39:33 +0900

    fix: modify instance name and description

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,18 @@ set -e
 
 if [ "$SSM_ACTIVATE" = "true" ]; then
   ACTIVATE_PARAMETERS=$(aws ssm create-activation \
-    --default-instance-name "$RESOURCE_STAGE-$SERVICE_NAME-$RESOURCE_VERSION" \
-    --description "$RESOURCE_STAGE-$SERVICE_NAME-$RESOURCE_VERSION" \
+    --default-instance-name bastion \
+    --description bastion \
     --iam-role "service-role/AmazonEC2RunCommandRoleForManagedInstances" \
+    --tags "Key=From,Value=selfish" \
+    --region ap-northeast-1 \
     --registration-limit 5)
 
-  export ACTIVATE_CODE=$(echo $ACTIVATE_PARAMETERS | jq -r .ActivationCode)
-  export ACTIVATE_ID=$(echo $ACTIVATE_PARAMETERS | jq -r .ActivationId)
+  ACTIVATE_CODE=$(echo $ACTIVATE_PARAMETERS | jq -r .ActivationCode)
+  ACTIVATE_ID=$(echo $ACTIVATE_PARAMETERS | jq -r .ActivationId)
   amazon-ssm-agent -register -code "${ACTIVATE_CODE}" -id "${ACTIVATE_ID}" -region "ap-northeast-1" -y
-  nohup amazon-ssm-agent > /dev/null &
+
+  echo "activateId is ${ACTIVATE_ID}"
+
+  amazon-ssm-agent
 fi
